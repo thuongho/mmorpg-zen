2255cc8b29741ea9b6adde192e69471df290427b
Thuong Ho <thuong.t.ho@gmail.com>
2019-11-17 17:48:37 -0800

    add title screen

--- assets/js/scenes/BootScene.js
+++ assets/js/scenes/BootScene.js
@@ -8,6 +8,7 @@ class BootScene extends Phaser.Scene {
     // tells phaser to load in some type of asset
     // this image method takes (key, location)
     this.load.image('button1', 'assets/images/ui/blue_button01.png');
+    this.load.image('button2', 'assets/images/ui/blue_button02.png');
     // set frame, margin
     this.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHeight: 32});
     this.load.spritesheet('characters', 'assets/images/characters.png', { frameWidth: 32, frameHeight: 32 });
@@ -16,6 +17,6 @@ class BootScene extends Phaser.Scene {
 
   create() {
     // transition from Boot to Game
-    this.scene.start('Game');
+    this.scene.start('Title');
   }
 }
--- assets/js/scenes/TitleScene.js
+++ assets/js/scenes/TitleScene.js
@@ -4,11 +4,37 @@ class TitleScene extends Phaser.Scene {
     super('Title');
   }
 
-  preload() {
-
-  }
-
   create() {
-    
+    // create title text
+    // x, y, text, config (size, color)
+    this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Sam MMORPG', { fontSize: '64px', fill: '#FFFFFF'});
+
+    this.titleText.setOrigin(0.5);
+    this.button = this.add.image(this.scale.width / 2, this.scale.height * 0.65, 'button1');
+    // button.setOrigin(0.5, 0.5);
+  
+    // // sprites can animate
+    // this.add.sprite(300, 100, 'button1');
+    // make interactive to get events
+    this.button.setInteractive();
+
+    // add button text
+    this.buttonText = this.add.text(0, 0, 'Start', { fontSize: '26px', fill: '#FFF'});
+    // add text in button
+    Phaser.Display.Align.In.Center(this.buttonText, this.button);
+
+    // events to have a hoover effect
+    this.button.on('pointerdown', () => {
+      console.log('pointer down');
+      this.scene.start('Game');
+    });
+    this.button.on('pointerover', () => {
+      console.log('pointer over');
+      this.button.setTexture('button2');
+    });
+    this.button.on('pointerout', () => {
+      console.log('pointer out');
+      this.button.setTexture('button1');
+    });
   }
 }
