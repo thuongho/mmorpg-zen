870de361de952aea8d42d1565a8326de2b862c61
Thuong Ho <thuong.t.ho@gmail.com>
2020-01-12 22:24:53 -0800

    add weapon to player and change angle base on direction

--- assets/js/classes/player/PlayerContainer.js
+++ assets/js/classes/player/PlayerContainer.js
@@ -1,9 +1,20 @@
+const Direction = {
+  RIGHT: 'RIGHT',
+  LEFT: 'LEFT',
+  UP: 'UP',
+  DOWN: 'DOWN'
+};
+
 class PlayerContainer extends Phaser.GameObjects.Container {
   constructor(scene, x, y, key, frame) {
     super(scene, x, y);
     this.scene = scene;
     // velocity when moving our player
     this.velocity = 160;
+    this.currentDirection = Direction.RIGHT;
+    this.playerAttacking = false;
+    this.flipX = true;
+    this.swordHit = false;
 
     // set size on container, by default container doesn't have size until items are added to it
     this.setSize(64, 64);
@@ -25,6 +36,18 @@ class PlayerContainer extends Phaser.GameObjects.Container {
     this.player = new Player(this.scene, 0, 0, key, frame);
     // add player to this container
     this.add(this.player);
+
+    // creat weapon grame object
+    this.weapon = this.scene.add.image(40, 0, 'items', 4);
+    // add weapon to existing scene
+    this.scene.add.existing(this.weapon);
+    // scale the weapon
+    this.weapon.setScale(1.5);
+    // add physics to weapon
+    this.scene.physics.world.enable(this.weapon);
+    this.add(this.weapon);
+    // only show the weapon when moving
+    this.weapon.alpha = 1;
   }
 
   // update is not called automatically
@@ -35,14 +58,34 @@ class PlayerContainer extends Phaser.GameObjects.Container {
   
     if (cursors.left.isDown) {
       this.body.setVelocityX(-this.velocity);
+      this.currentDirection = Direction.LEFT;
+      this.weapon.setPosition(-40, 0);
     } else if (cursors.right.isDown) {
       this.body.setVelocityX(this.velocity);
+      this.currentDirection = Direction.RIGHT;
+      this.weapon.setPosition(40, 0);
     }
   
     if (cursors.up.isDown) {
       this.body.setVelocityY(-this.velocity);
+      this.currentDirection = Direction.UP;
+      this.weapon.setPosition(0, -40);
     } else if (cursors.down.isDown) {
       this.body.setVelocityY(this.velocity);
+      this.currentDirection = Direction.DOWN;
+      this.weapon.setPosition(0, 40);
+    }
+
+    if (this.playerAttacking) {
+
+    } else {
+      if (this.currentDirection === Direction.DOWN) {
+        this.weapon.setAngle(-270);
+      } else if (this.currentDirection === Direction.UP || this.currentDirection === Direction.LEFT) {
+        this.weapon.setAngle(-90);
+      } else {
+        this.weapon.setAngle(0);
+      }
     }
   }
 }
